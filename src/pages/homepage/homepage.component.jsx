36a841be99c91aa776fcd0969872d65c1acca634
rajp6265 <rajp6265.rp@gmail.com>
2023-01-03 00:21:42 -0500

    homepage - menuitems

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directorty.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default HomePage;
